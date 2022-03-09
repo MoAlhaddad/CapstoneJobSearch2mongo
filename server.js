ac57cc7ded06802edbee3eddd15d8b28c1b09126
MoAlhaddad <mohamadalhaddad25@gmail.com>
2022-03-08 22:19:16 -0800

    added routes setup MongoDb and Postman

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
 const app = express();
-const db = require("./db.js")
+const db = require("./db.js");
+const jobsRoute = require("./routes/jobsRoute");
+
+app.use('/api/jobs/' , jobsRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Node JS Server started'));
+app.listen(port, () => console.log("Node JS Server started"));
